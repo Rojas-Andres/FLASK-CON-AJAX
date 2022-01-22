4bb012cf4029ebdd13c75e2cdd7aa282ed3287cc
Rojas-Andres <andresfelipe200004@gmail.com>
2022-01-22 16:42:32 -0500

    Explicacion ajax

---
+++ AJAX_FLASK/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/solicitaDatos',methods=["GET"])
+def solicitaDatos():
+    return jsonify({"frutas":["pera","manzana","mandarina"]})
+
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0")
